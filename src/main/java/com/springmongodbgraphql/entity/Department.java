a71f0b145c7aefa6c8e59b5b0e237a705a666f6b
Ibrahim Olanrewaju <semslam007@gmail.com>
2022-07-01 01:44:24 +0100

    add lombok lib for getter and setter

--- src/main/java/com/springmongodbgraphql/entity/Department.java
+++ src/main/java/com/springmongodbgraphql/entity/Department.java
@@ -10,21 +10,4 @@ public class Department {
 	@Getter @Setter private String departmentName;
 
 	@Getter @Setter private String location;
-
-	public String getDepartmentName() {
-		return departmentName;
-	}
-
-	public void setDepartmentName(String departmentName) {
-		this.departmentName = departmentName;
-	}
-
-	public String getLocation() {
-		return location;
-	}
-
-	public void setLocation(String location) {
-		this.location = location;
-	}
-
 }
--- src/main/java/com/springmongodbgraphql/entity/Subject.java
+++ src/main/java/com/springmongodbgraphql/entity/Subject.java
@@ -1,29 +1,16 @@
 package com.springmongodbgraphql.entity;
 
 import org.springframework.data.mongodb.core.mapping.Field;
+import lombok.Getter;
+import lombok.Setter;
 
 public class Subject {
 
 	@Field(name = "subject_name")
-	private String subjectName;
+	@Getter @Setter private String subjectName;
 
 	@Field(name = "marks_obtained")
-	private int marksObtained;
+	@Getter @Setter private int marksObtained;
 
-	public String getSubjectName() {
-		return subjectName;
-	}
-
-	public void setSubjectName(String subjectName) {
-		this.subjectName = subjectName;
-	}
-
-	public int getMarksObtained() {
-		return marksObtained;
-	}
-
-	public void setMarksObtained(int marksObtained) {
-		this.marksObtained = marksObtained;
-	}
 
 }
